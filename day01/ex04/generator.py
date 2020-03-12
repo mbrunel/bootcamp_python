abb39397859aad93fca5f4f982674ab9f28d96a3
Matthias BRUNEL <mbrunel@e3r9hsu.fr>
2020-03-12 03:29:49 +0100

    add error handling

--- day01/ex04/generator.py
+++ day01/ex04/generator.py
@@ -6,7 +6,7 @@
 #    By: mbrunel <marvin@42.fr>                     +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/03/12 02:58:30 by mbrunel           #+#    #+#              #
-#    Updated: 2020/03/12 03:17:39 by mbrunel          ###   ########.fr        #
+#    Updated: 2020/03/12 03:28:42 by mbrunel          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,10 +14,15 @@ import random
 
 def generator(text, sep=" ", option=None):
 	'''Option is an optional arg, sep is mandatory'''
+	if (type(text) != str or (option != None and option != "ordered" and option != "shuffle")):
+		yield "ERROR"
+		return
+
 	l = text.split(sep)
 	if option == "ordered":
 		l.sort()
 	if option == "shuffle":
 		random.shuffle(l)
+	
 	for word in l:
 		yield word
